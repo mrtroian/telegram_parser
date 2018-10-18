0343d5cc82f2e68414f6920ee6104e91016a2732
Cyril Troian <cyril@mrtroian.local>
2018-10-18 16:20:33 +0300

    major optimisation and refactoring

--- telegram-parser/parser.py
+++ telegram-parser/parser.py
@@ -1,43 +1,52 @@
 from sys import argv
-from parsing import parse_messages, parse_participants, telegram_init, get_datum_range, get_message_range
+from parsing import Parsing
+from datetime import datetime
 from config import keys
 
 def main():
 	try:
-		if len(argv) < 2:
-			print("Options:\nparse_messages\nparse_participants\nget_statistics")
-		elif argv[1] == "parse_messages":
-			if len(argv) < 4:
-				print("Missing argument: [group, number of messages]")
-				return 0
-			parse_messages(telegram_init(), argv[2], int(argv[3]))
+		if len(argv) < 3:
+			print('''Options:\nparse_messages [group name]
+				\rparse_participants [group name]\nget_statistics [group name | date from | date until]''')
+			return 1
+		parse = Parsing(argv[2])
+		if argv[1] == "parse_messages":
+			if len(argv) == 3:
+				mod = 0
+			elif len(argv) == 4:
+				mod = int(argv[3])
+			elif len(argv) == 5:
+				mod = list()
+				mod.append(datetime.strptime(argv[3], '%Y-%m-%d'))
+				mod.append(datetime.strptime(argv[4], '%Y-%m-%d'))
+			else:
+				return 1
+			parse.parse_messages(mod)
 		elif argv[1] == "parse_participants":
-			if len(argv) < 3:
-				print("Missing argument: [group]")
-				return 0
-			parse_participants(telegram_init(), argv[2])
+			parse.parse_participants()
 		elif argv[1] == "get_statistics":
-			if len(argv) < 4:
-				print("Missing arguments: [date from, date until]")
-				return 0
-			datum_range = get_datum_range(argv[2], argv[3])
+			if len(argv) < 5:
+				print('Invalid argument')
+				return 1
+			datum_range = parse.get_datum_range(argv[3], argv[4])
 			if datum_range:
-				print("There are %i messages during this period.\nDo you want to proceed?[y/n]" % datum_range)
+				print("There are %i messages during this period.\nDo you want to proceed?[y/n]" % datum_range[0])
 			else:
 				print("There are no messages during this period.")
 				return
 			if 'y' in input():
-				get_message_range(argv[2], argv[3])
-			else:
-				return 0
-		else:
-			print("Invalid argument")
-			print("Options:\nparse_messages\nparse_participants\nget_statistics")
-			return 0
-		return 1
+				parse.get_message_range(argv[3], argv[4], datum_range[1])
+				print('Done')
+		parse.finish()
+		return 0
+	except KeyboardInterrupt:
+		print('Terminating...', end='\r')
+		parse.finish()
+		print('Terminated...')
+		return 0
 	except:
-			print("Invalid argument")
-			return 0
+		print("Invalid argument")
+		return 0
 
 if __name__ == "__main__":
 	main()
--- telegram-parser/parsing.py
+++ telegram-parser/parsing.py
@@ -1,170 +1,297 @@
 from telethon import TelegramClient, sync
 from azure_parser import get_azure_data
+from datetime import datetime, timedelta
 from config import keys
 import pymysql
 
-# Обьект для работы с SQL
-def sql_init():
-	return pymysql.connect(keys['db_host'], keys['db_user'], keys['db_password'], keys['db_name'])
 
-# Обьект для работы с Telegram
-def telegram_init():
-	client = TelegramClient(keys['user'], keys['ID'], keys['HASH'])
-	return client.start()
-
-
-def get_user_seen(uid):
-	db = sql_init()
-	cursor = db.cursor()
-	cursor.execute("SELECT MAX(Date) FROM Messages WHERE FromID = %i" % (uid))
-	lastdate = cursor.fetchone()
-	cursor.execute("SELECT MIN(Date) FROM Messages WHERE FromID = %i" % (uid))
-	firstdate = cursor.fetchone()
-	return (lastdate[0], firstdate[0])
+class Parsing(object):
+	def __init__(self, group_name):
+		self.db = pymysql.connect(keys['db_host'], keys['db_user'], keys['db_password'], keys['db_name'])
+		self.cursor = self.db.cursor()
+		self.client = TelegramClient(keys['user'], keys['ID'], keys['HASH']).start()
+		self.group_ent = self.client.get_entity(group_name)
 
 
-def get_row(row, table, uid=0):
-	db = sql_init()
-	cursor = db.cursor()
-	if uid:
-		cursor.execute("SELECT %s FROM %s WHERE ID == %s" % (row, table, uid))
-	else:
-		cursor.execute("SELECT %s FROM %s" % (row, table))
-	rows = cursor.fetchall()
-	res = list()
-	for x in rows:
-		res.append(x[0])
-	return res
+	def __get_user_seen(self, table, part_table, uid):
+		self.cursor.execute("SELECT MIN(Date), MAX(Date) FROM %s WHERE FromID = %i" % (table, uid))
+		m_dates = self.cursor.fetchall()
+		self.cursor.execute("SELECT First_Date_Seen, Last_Date_Seen FROM %s WHERE User_ID = %i" 
+			% (part_table, uid))
+		c_dates = self.cursor.fetchall()
+		return (m_dates, c_dates)
 
 
-def get_userID_by_username(client, message):
-	# Если есть упоминание по username, получаем ID
-	for el in message:
-		if '@' in el:
-			try:
-				return client.get_entity(el).id
-			except:
-				pass
-	return
+	def __int(self, fl):
+		return 1 if fl >= 0.5 else 0
 
 
-def get_datum_range(date_from, date_until):
-	db = sql_init()
-	cursor = db.cursor()
-	cursor.execute("SELECT count(ID) from Messages WHERE date >= '%s' and date <= '%s'" % (date_from, date_until))
-	rows = cursor.fetchone()
-	return rows[0]
-
-
-def get_message_range(date_from, date_until):
-	db = sql_init()
-	cursor = db.cursor()
-	cursor.execute("SELECT ID, message from Messages WHERE date >= '%s' and date <= '%s'" % (date_from, date_until))
-	rows = cursor.fetchall()
-	table = 'Messages'
-	for elem in rows:
-		azure_data = get_azure_data(elem[1])
-		if azure_data[0]:
-			sql_push(table, elem[0], 'Language', azure_data[0])
-		sql_push(table, elem[0], 'sentiment', int(azure_data[1]))
-		if azure_data[2]:
-			sql_push(table, elem[0], 'key_phrases', azure_data[2])
-		if azure_data[3]:
-			sql_push(table, elem[0], 'entities', azure_data[3])
-		break
-
-# Функция для работы с SQL
-def sql_push(table, mid, col=0, data=-1, tid='ID'):
-	db = sql_init()
-	cursor = db.cursor()
-	# Если data строка, обновляем в базе
-	if col and isinstance(data, str):
-		if "'" in data: # На случай, если в сообщении есть кавычки
-			query = ''' UPDATE %s SET %s = "%s" WHERE %s = %s''' % (table, col, data, tid, mid)
+	def __s_min(self, val1, val2):
+		if val1 and val2:
+			return val1 if val1 < val2 else val2
 		else:
-			query = ''' UPDATE %s SET %s = '%s' WHERE %s = %s''' % (table, col, data, tid, mid)
-	elif col and isinstance(data, int):
-		if data >= 0:
-			query = '''UPDATE %s SET %s = %i WHERE %s = %s''' % (table, col, data, tid, mid)
-	# Если col равнен нулю и data -1 - такого ряда в базе еще нет создаем новый ряд
-	# data по умолчанию -1, так как sentiment равен нулю или единице
-	elif not col and data == -1:
-		query = '''INSERT INTO %s(%s) VALUES(%s)''' % (table, tid, mid)
-	else:
+			if val1:
+				return val1
+			elif val2:
+				return val2
+
+
+	def __table_exist(self, table):
+		try:
+			self.cursor.execute("SELECT 1 FROM %s LIMIT 1;" % (table))
+			return True
+		except:
+			return False
+
+
+	def __get_forward_table(self):
+		if hasattr(self.group_ent, 'username'):
+			tablename = self.group_ent.username + '_Forward'
+		elif hasattr(self.group_ent, 'id'):
+			tablename = self.group_ent.id + '_Forward'
+		else:
+			print("group Error")
+			return False
+		if not self.__table_exist(tablename):
+			self.cursor.execute(
+				''' CREATE TABLE %s (
+				    ID INT,
+				    OriginalDate DATE,
+				    FromID INT,
+				    ChannelPost INT
+				)''' % (tablename)
+			)
+			print("%s table created" % tablename)
+		return tablename
+
+
+	def __get_participants_table(self):
+		if hasattr(self.group_ent, 'username'):
+			tablename = self.group_ent.username + '_ChatParticipants'
+		elif hasattr(self.group_ent, 'id'):
+			tablename = self.group_ent.id + '_ChatParticipants'
+		else:
+			print("group Error")
+			return False
+		if not self.__table_exist(tablename):
+			self.cursor.execute(
+				''' CREATE TABLE %s (
+				    User_ID INT,
+				    First_Date_Seen DATE,
+				    Last_Date_Seen DATE
+				);''' % (tablename)
+			)
+			print("%s table created" % tablename)
+		return tablename
+
+
+	def __get_messages_table(self):
+		if hasattr(self.group_ent, 'username'):
+			tablename = self.group_ent.username + '_Messages'
+		elif hasattr(self.group_ent, 'id'):
+			tablename = self.group_ent.id + '_Messages'
+		else:
+			print("group Error")
+			return False
+		if not self.__table_exist(tablename):
+			self.cursor.execute(
+				''' CREATE TABLE %s (
+				    ID INT,
+				    Date DATE,
+				    FromID INT,
+				    Message TEXT,
+				    ReplyMessageID INT,
+				    ViewCount INT,
+				    Formatting TEXT,
+				    ServiceAction TEXT,
+				    Language TEXT,
+				    key_phrases TEXT,
+				    entities TEXT,
+				    sentiment INT
+				);''' % (tablename)
+			)
+			print("%s table created" % tablename)
+		return tablename
+
+
+	def __get_row(self, row, table, uid=0):
+		if uid:
+			self.cursor.execute("SELECT %s FROM %s WHERE ID == %s" % (row, table, uid))
+		else:
+			self.cursor.execute("SELECT %s FROM %s" % (row, table))
+		rows = self.cursor.fetchall()
+		res = list()
+		for el in rows:
+			res.append(el[0])
+		return res
+
+
+	def __get_userID_by_username(self, message):
+		for el in message:
+			if '@' in el:
+				try:
+					return self.client.get_entity(el).id
+				except:
+					pass
 		return
-	try:
-		cursor.execute(query)
-		db.commit()
-	except:
-		db.rollback()
 
 
-def parse_participants(client, group):
-	users = client.iter_participants(group)
-	table = 'ChatParticipants'
-	uid = 'User_ID'
-	participants = get_row(uid, table)
-	i = 0
-	for user in users:
-		if not user.id in participants:
-			sql_push(table, user.id, 0, -1, uid)
-		dates = get_user_seen(user.id)
-		# Если есть значение в кортеже, заносим в базу
-		if dates[1]:
-			sql_push(table, user.id, 'First_Date_Seen', dates[1], uid)
-		if dates[0]:
-			sql_push(table, user.id, 'Last_Date_Seen', dates[0], uid)
-		print('\r%i' % i, end='')
-		i += 1
-	print()
+	def get_datum_range(self, date_from, date_until):
+		if hasattr(self.group_ent, 'username'):
+			table = self.group_ent.username + '_Messages'
+		elif hasattr(self.group_ent, 'id'):
+			table = self.group_ent.id + '_Messages'
+		else:
+			print("group Error")
+			return False
+		self.cursor.execute("SELECT count(ID) from %s WHERE date >= '%s' and date <= '%s'" 
+			% (table, date_from, date_until))
+		rows = self.cursor.fetchone()
+		return (rows[0], table)
 
 
-def parse_messages(client, group, last=20):
-	group_ent = client.get_entity(group) 	# получаем обьект группы
-	posts = client.iter_messages(group_ent) 	# получаем итератор сообщений
-	table = 'Messages'
-	ids = get_row('ID', table) 		# получаем список пользователей в бд
-	i = 1
-	for message in posts:
-		# получаем сообщения по порядку, от последнего
-		mid = message.id
-		if mid and message.message:
-			if not mid in ids: # если пользователя нету, заносим ID в базу
-				sql_push(table, mid)
-			sql_push(table, mid, 'Date', message.date.strftime('%Y-%m-%d'))
-			if message.from_id:
-				sql_push(table, mid, 'FromID', message.from_id)
-			if message.message:
-				sql_push(table, mid, 'Message', message.message)
-			if message.reply_to_msg_id:
-				sql_push(table, mid, 'ReplyMessageID', message.reply_to_msg_id)
-			if message.fwd_from:
-				sql_push(table, mid, 'ForwardID', message.fwd_from.from_id)
-			if message.views:
-				sql_push(table, mid, 'ViewCount', message.views)
-			if hasattr(message.media, 'webpage'):
-				if hasattr(message.media.webpage, 'url'):
-					sql_push(table, mid, 'Formatting', message.media.webpage.url)
-			if hasattr(message, 'entities'):
-				if message.entities and '@' in message.message:
-					sql_push(table, mid, 'Formatting', get_userID_by_username(client, message.message.split(' ')))
+	def get_message_range(self, date_from, date_until, table):
+		self.cursor.execute("SELECT ID, message from %s WHERE date >= '%s' and date <= '%s'" 
+			% (table, date_from, date_until))
+		rows = self.cursor.fetchall()
+		for elem in rows:
+			azure_data = get_azure_data(elem[1])
+			if azure_data[0]:
+				self.__sql_push(table, elem[0], 'Language', azure_data[0])
+			self.__sql_push(table, elem[0], 'sentiment', int(azure_data[1]))
+			if azure_data[2]:
+				self.__sql_push(table, elem[0], 'key_phrases', azure_data[2])
+			if azure_data[3]:
+				self.__sql_push(table, elem[0], 'entities', azure_data[3])
 
-			# Следующий блок кода предназначен для работы с azure api
 
-			# azure_data = get_azure_data(message.message) # получаем данные
-			# if azure_data[0]:
-			# 	sql_push(table, mid, 'Language', azure_data[0])
-			# sql_push(table, mid, 'sentiment', int(azure_data[1]))
-			# if azure_data[2]:
-			# 	sql_push(table, mid, 'key_phrases', azure_data[2])
-			# if azure_data[3]:
-			# 	sql_push(table, mid, 'entities', azure_data[3])
+	def __sql_push(self, table, mid, col=0, data=-1, tid='ID'):
+		if col and isinstance(data, str):
+			if "'" in data:
+				query = ''' UPDATE %s SET %s = "%s" WHERE %s = %s''' % (table, col, data, tid, mid)
+			else:
+				query = ''' UPDATE %s SET %s = '%s' WHERE %s = %s''' % (table, col, data, tid, mid)
+		elif col and isinstance(data, int):
+			if data >= 0:
+				query = '''UPDATE %s SET %s = %i WHERE %s = %s''' % (table, col, data, tid, mid)
+		elif not col and data == -1:
+			query = '''INSERT INTO %s(%s) VALUES(%s)''' % (table, tid, mid)
+		else:
+			return
+		try:
+			self.cursor.execute(query)
+			self.db.commit()
+		except:
+			self.db.rollback()
 
-		if hasattr(message, 'action'):
-			if message.action: # Системные сообщения (добавление пользователя, итп)
-				sql_push(table, mid, 'ServiceAction', str(message.action))
-		if i >= last:
-			print('\r%i' % i)
-			break
-		print('\r%i' % i, end='')
-		i += 1
+
+	def finish(self):
+		self.db.close()
+
+
+	def parse_participants(self):
+		users = self.client.iter_participants(self.group_ent)
+		new_users = self.client.iter_participants(self.group_ent)
+		table = self.__get_participants_table()
+		mess_table = self.__get_messages_table()
+		uid = 'User_ID'
+		participants = self.__get_row(uid, table)
+		sql = self.__sql_push
+		for user in new_users:
+			active_user = user.id in participants
+			if not active_user:
+				if not participants:
+					sql(table, user.id, 0, -1, uid)
+				else:
+					sql(table, user.id, 0, -1, uid)
+		i = 0
+		participants = self.__get_row(uid, table)
+		for user in users:
+			active_user = user.id in participants
+			date = self.__get_user_seen(mess_table, table, user.id)
+			if active_user:
+				if date[0][0][0] or date[1][0][0]:
+					sql(table, user.id, 'First_Date_Seen', self.__s_min(date[0][0][0],
+						date[1][0][0]).strftime('%Y-%m-%d'), uid)
+				else:
+					sql(table, user.id, 'First_Date_Seen', datetime.now().strftime('%Y-%m-%d'), uid)
+				sql(table, user.id, 'Last_Date_Seen', datetime.now().strftime('%Y-%m-%d'), uid)
+			print('\r%i' % i, end='')
+			i += 1
+		print()
+
+
+	def parse_messages(self, mod=0):
+		posts = self.client.iter_messages(self.group_ent)
+		table = self.__get_messages_table()
+		forward_table = self.__get_forward_table()
+		ids = self.__get_row('ID', table)
+		sql = self.__sql_push
+		if not table:
+			return
+		if isinstance(mod, int) and mod:
+			last = True
+		else:
+			last = False
+		if isinstance(mod, list):
+			fdate = mod[0] + timedelta(hours=0)
+			ldate = mod[1] + timedelta(hours=24)
+			flag = True
+		else:
+			flag = False
+		i = 1
+		for message in posts:
+			nf = True
+			mid = message.id
+			if flag:
+				mdate = message.date.replace(tzinfo=None)
+				nf = True if mdate >= fdate and mdate <= ldate else False
+				if mdate < fdate:
+					print()
+					break
+			if mid and nf:
+				if not mid in ids:
+					sql(table, mid)
+				sql(table, mid, 'Date', message.date.strftime('%Y-%m-%d'))
+				if hasattr(message, 'action'):
+					if message.action:
+						sql(table, mid, 'ServiceAction', str(message.action))
+				if message.from_id:
+					sql(table, mid, 'FromID', message.from_id)
+				if hasattr(message, 'message'):
+					if message.message:
+						sql(table, mid, 'Message', message.message)
+					if message.reply_to_msg_id:
+						sql(table, mid, 'ReplyMessageID', message.reply_to_msg_id)
+					if message.fwd_from:
+						sql(forward_table, mid)
+						sql(forward_table, mid, 'OriginalDate', message.date.strftime('%Y-%m-%d'))
+						sql(forward_table, mid, 'FromID', message.fwd_from.from_id)
+						sql(forward_table, mid, 'ChannelPost', self.group_ent.id)
+						sql(table, mid, 'ForwardID', message.fwd_from.from_id)
+					if message.views:
+						sql(table, mid, 'ViewCount', message.views)
+					if hasattr(message.media, 'webpage'):
+						if hasattr(message.media.webpage, 'url'):
+							sql(table, mid, 'Formatting', message.media.webpage.url)
+					if hasattr(message, 'entities'):
+						if message.entities and '@' in message.message:
+							sql(table, mid, 'Formatting',
+								self.__get_userID_by_username(message.message.split(' ')))
+
+				# azure_data = get_azure_data(message.message)
+				# print(azure_data)
+				# if azure_data[0]:
+				# 	sql(table, mid, 'Language', azure_data[0])
+				# sql(table, mid, 'sentiment', self.__int(azure_data[1]))
+				# if azure_data[2]:
+				# 	sql(table, mid, 'key_phrases', azure_data[2])
+				# if azure_data[3]:
+				# 	sql(table, mid, 'entities', azure_data[3])
+
+			if last:
+				if i >= mod:
+					print('\r%i' % i)
+					break
+			print('\r%i' % i, end='')
+			i += 1
